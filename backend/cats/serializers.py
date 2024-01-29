306709eb286cfb07e1ebcffc7fde96a6ce3fde07
Shukurillo Karimov <gratefultolord@gmail.com>
2024-01-30 00:51:51 +0300

    Second review

--- backend/cats/serializers.py
+++ backend/cats/serializers.py
@@ -9,6 +9,7 @@ from .models import Achievement, AchievementCat, Cat
 
 
 class Hex2NameColor(serializers.Field):
+
     def to_representation(self, value):
         return value
 
@@ -29,12 +30,13 @@ class AchievementSerializer(serializers.ModelSerializer):
 
 
 class Base64ImageField(serializers.ImageField):
+
     def to_internal_value(self, data):
         if isinstance(data, str) and data.startswith('data:image'):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
 
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+            data = ContentFile(base64.b64decode(imgstr), name=f'temp.{ext}')
 
         return super().to_internal_value(data)
 
